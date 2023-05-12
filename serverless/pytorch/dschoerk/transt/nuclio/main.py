710e40fdc117b6a3c87fe58d500a1389d0a017ba
zhitm <zhtinuhina.maria@gmail.com>
2023-05-13 02:40:46 +0300

    http communication

--- serverless/pytorch/dschoerk/transt/nuclio/main.py
+++ serverless/pytorch/dschoerk/transt/nuclio/main.py
@@ -2,37 +2,62 @@
 import base64
 import io
 import json
+import requests
+from io import BytesIO
+import base64
 
 import numpy as np
-from model_handler import ModelHandler
+from model_handler import ModelState
 from PIL import Image
+
+url = 'http://192.168.0.107:8083'
+
+
+def image_to_base64(img):
+    im_file = BytesIO()
+    img.save(im_file, format="JPEG")
+    im_bytes = im_file.getvalue()  # im_bytes: image in binary format.
+    im_b64 = base64.b64encode(im_bytes).decode('utf-8')
+    return im_b64
 
 
 def init_context(context):
     context.logger.info("Init context...  0%")
-    model = ModelHandler()
+    # requests.get('localhost:8083')
+    model = ModelState()
     context.user_data.model = model
     context.logger.info("Init context...100%")
 
 def handler(context, event):
 
-    context.logger.info("Run TransT model")
     data = event.body
     buf = io.BytesIO(base64.b64decode(data["image"]))
     shapes = data.get("shapes")
     states = data.get("states")
 
     image = Image.open(buf).convert('RGB')
-    image = np.array(image)[:, :, ::-1].copy()
+    shape = shapes[0]
+    data = {'image': image_to_base64(image), 'x': shape[0], 'y': shape[1], 'x1': shape[2], 'y1': shape[3]}
 
-    results = {
-        'shapes': [],
-        'states': []
-    }
-    for i, shape in enumerate(shapes):
-        shape, state = context.user_data.model.infer(image, shape, states[i] if i < len(states) else None)
-        results['shapes'].append(shape)
-        results['states'].append(state)
+    results={}
+    if len(states) == 0:
+        requests.post(url, json=data)
 
+        results = {
+            'shapes': [shape],
+            'states': []
+        }
+    else:
+        r = requests.get(url, json=data)
+        j = r.json()
+        x=j["x"]
+        y=j["y"]
+        width=j['width']
+        height=j['height']
+        results = {
+            'shapes': [[x,y,x+width,y+height]],
+            'states': []
+        }
     return context.Response(body=json.dumps(results), headers={},
         content_type='application/json', status_code=200)
+
